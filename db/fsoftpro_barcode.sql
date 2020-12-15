3e1a346c8b83da128887935d4650b84b5f3af64b
munkgorn <munk.gorn@gmail.com>
2020-12-15 23:07:58 +0700

    Create fsoftpro_barcode.sql

---
+++ db/fsoftpro_barcode.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100414
+ Source Host           : localhost:3306
+ Source Schema         : fsoftpro_barcode
+
+ Target Server Type    : MySQL
+ Target Server Version : 100414
+ File Encoding         : 65001
+
+ Date: 15/12/2020 22:48:47
+*/
+
